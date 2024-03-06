befdf3f604e76fbbbd684b875a235e6d87e7f6a3
RahwaLegesse <rahwalegesse31@gmail.com>
2024-03-06 10:10:14 +0000

    number 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,18 @@
-#!/usr/bin/python3        
-"""
-If an invalid subreddit is given,
-the function should return 0.
-"""
+#!/usr/bin/python3
+"""nd the number of subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    ''' get numberbcribers'''
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
+    url = 'http://reddit.com/r/{}/about.json'
+    headers = {'User-agent': 'colombiandreamm'}
+    response = requests.get(url.format(subreddit), headers=headers)
+    if response.status_code == 200:
+        subs = response.json()
+        '''subs0 = subs['data']['subscribers']'''
+        subs0 = subs.get('data').get('subscribers')
+        return subs0
     else:
-        return (0)
+        return 0
